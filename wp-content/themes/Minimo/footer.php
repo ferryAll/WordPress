455b3afa6f642d7fa099160d4b3a4468ab9a3d2a
Ferry <allan.ferry@orange.fr>
2019-01-15 09:32:34 +0100

    yjk

--- wp-content/themes/Minimo/footer.php
+++ wp-content/themes/Minimo/footer.php
@@ -1,6 +1,14 @@
+<?php wp_footer(); ?>
 
-  
-
-    <?php wp_footer(); ?>  
-</body>
+    <ul class="vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown">
+        <li class="li"><a href= "#" > Mentions Légales</a></li>
+        <li class="li"><a href= "#" > Contact</a></li>
+ 
+    </ul>  
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/vendor/foundation.js"></script>
+    <script src="assets/js/app.js"></script>
+    </div>
+  </body>
 </html>
--- wp-content/themes/Minimo/header.php
+++ wp-content/themes/Minimo/header.php
@@ -2,6 +2,39 @@
 
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
-  <head>  
-    <body> 
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minimo</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+  </head>
+
+  <body> 
     
+  <div class="grid-container">
+  <img src="assets/images/logo_minimo.png"><img/>
+  <ul class="dropdown menu" data-dropdown-menu>
+  <li><a href="Accueil.php">Accueil</a></li>
+  <li>
+    <a href="Article.php">Articles</a>
+    <ul class="menu">
+      <li><a href="#">Lifestyle</a></li>
+      <li><a href="#">Photos</a></li>
+      <li><a href="#">Musique</a></li>
+      <li><a href="#">Visites</a></li>
+      <!-- ... -->
+    </ul>
+  </li>
+  <li>
+    <a href="#"> Déjà Membre? </a>
+      <ul class="menu">
+        <li><a href="#">Connexion</a></li>
+        <li><a href="#">Inscription</a></li>
+      </ul>
+  </li>
+
+    
+  <img class="main_image" src="assets/images/01_image_principale.png"><img/>
+  
