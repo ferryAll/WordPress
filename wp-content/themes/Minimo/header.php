50de192250a37f4d31bc35d8a27fa2a181da734f
Ferry <allan.ferry@orange.fr>
2019-01-15 09:19:29 +0100

    ru

--- wp-content/themes/Minimo/header.php
+++ wp-content/themes/Minimo/header.php
@@ -2,8 +2,6 @@
 
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
-  <head>
-
-  <img class="main_image" src="assets/images/01_image_principale.png"><img/>
-  
-  <body> 
+  <head>  
+    <body> 
+    
